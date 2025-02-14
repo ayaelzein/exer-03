212860207b68ba6aa0052bf9ffffab505db44a84
ayaelzein <aya.m.elzein@gmail.com>
2025-02-14 14:18:19 +0200

    user and pass check

--- ex-03.py
+++ ex-03.py
@@ -1,6 +1,9 @@
 print("Please enter your user and password below to login")
 
 user = input("Enter Username here: ")
+password = input("Enter Password here: ")
 
-if user == "admin":
+if user == "admin" and password == 1234:
     print("Access Granted")
+else:
+    print("Access Denied")
